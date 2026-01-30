19ae29bf118a4c307112f48ca1b90c1d8084aac9
Megatron8000 <barykin88@hotmail.com>
2026-01-30 13:35:32 +0400

    add all entities

---
+++ src/main/java/org/example/entity/SavingsAccountDetails.java
@@ -0,0 +1,31 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "savings_account_details")
+@Getter
+@Setter
+public class SavingsAccountDetails {
+
+    @Id
+    private UUID accountId;
+
+    @OneToOne
+    @MapsId
+    private Account account;
+
+    private BigDecimal interestRate;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private LocalDate lastCapitalizationDate;
+}
+
---
+++ src/main/java/org/example/entity/Transaction.java
@@ -0,0 +1,39 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.enums.TransactionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+public class Transaction {
+
+    @Id
+    private UUID id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Account account;
+
+    @ManyToOne
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    private BigDecimal amount;
+
+    private LocalDate transactionDate;
+
+    private boolean deleted;
+}
+
